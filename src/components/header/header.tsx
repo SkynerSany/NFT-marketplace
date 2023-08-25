f057c6e6ae1d5e501d8abf4a5645c7ea6a8cf71a
Skyner <skynerstudent@gmail.com>
2023-08-25 15:13:29 +0300

    feat: add search and second line in header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,43 +1,64 @@
+'use client'
+
 import Navigation from "./navigation";
 import './header.scss';
 import Link from "next/link";
 import Image from "next/image";
 import UserIcon from "../user-icon/user-icon";
+import { usePathname } from 'next/navigation';
+import Search from "../search/search";
 
 export default function Header() {
-  
+  const pathname = usePathname();
+
+  function isMainPage() {
+    return pathname === '/';
+  }
+
   return (
     <header className="header">
-      <div className="wrapper">
-        <Link href='/' className="logo">
-          <Image 
-            src="/assets/images/Logo.svg" 
-            alt="logo"
-            width={100}
-            height={100}
-          />
-          <span>CryptoBit</span>
-        </Link>
-        <Navigation />
-        <div className="user">
-          <div className="user__tools">
-            <Link href='/search' className="user__button user__button-search"></Link>
-            <Link href='/cart' className="user__button user__button-cart"></Link>
-            <Link href='/favorite' className="user__button user__button-favourite"></Link>
-            <Link href='/notification' className="user__button user__button-notification"></Link>
-          </div>
-          <div className="user__info">
-            <div className="user__info-container">
-              <p className="user__name">Jos Shark</p>
-              <p className="user__balance">
-                <span className="user__balance-number">123.45</span>
-                <span className="user__balance-currency">eth</span>
-              </p>
+      <div className="first-line">
+        <div className="wrapper">
+          <Link href='/' className="logo">
+            <Image 
+              src="/assets/images/Logo.svg" 
+              alt="logo"
+              width={100}
+              height={100}
+            />
+            <span>CryptoBit</span>
+          </Link>
+          {
+            isMainPage() ? <Navigation /> : <Search />  
+          }
+          <div className="user">
+            <div className="user__tools">
+              <Link href='/search' className="user__button user__button-search"></Link>
+              <Link href='/cart' className="user__button user__button-cart"></Link>
+              <Link href='/favorite' className="user__button user__button-favourite"></Link>
+              <Link href='/notification' className="user__button user__button-notification"></Link>
             </div>
-            <UserIcon link="/user" image="/assets/images/user-icon.svg" />
+            <div className="user__info">
+              <div className="user__info-container">
+                <p className="user__name">Jos Shark</p>
+                <p className="user__balance">
+                  <span className="user__balance-number">123.45</span>
+                  <span className="user__balance-currency">eth</span>
+                </p>
+              </div>
+              <UserIcon link="/user" image="/assets/images/user-icon.svg" />
+            </div>
           </div>
         </div>
       </div>
+      {
+        !isMainPage() && 
+        <div className="second-line">
+          <div className="wrapper">
+            <Navigation />
+          </div>
+        </div>
+      }
     </header>
   )
 };
--- src/components/header/navigation.tsx
+++ src/components/header/navigation.tsx
@@ -1,16 +1,53 @@
 import Link from "next/link";
+import { usePathname } from 'next/navigation';
+const NAV_LINKS = [
+  {
+    href: '/',
+    name: 'Home'
+  },
+  {
+    href: '/explore',
+    name: 'Explore'
+  },
+  {
+    href: '/creators',
+    name: 'Creators'
+  },
+  {
+    href: '/collectors',
+    name: 'Collectors'
+  },
+  {
+    href: '/product',
+    name: 'Product'
+  },
+  {
+    href: '/all-pages',
+    name: 'All Pages'
+  },
+  {
+    href: '/blog',
+    name: 'Blog'
+  },
+]
 
 export default function Navigation() {
-  
+  const pathname = usePathname()
+
   return (
     <nav className="navigation">
-      <Link href={'/'}>Home</Link>
-      <Link href={'/explore'}>Explore</Link>
-      <Link href={'/creators'}>Creators</Link>
-      <Link href={'/collectors'}>Collectors</Link>
-      <Link href={'/product'}>Product</Link>
-      <Link href={'/all-pages'}>All Pages</Link>
-      <Link href={'/blog'}>Blog</Link>
+      {
+        NAV_LINKS.map((link) => {
+          const isActive = pathname === link.href;
+          return <Link 
+            href={link.href} 
+            className={ isActive ? 'active' : '' }
+            key={link.name}
+          >
+            {link.name}
+          </Link>
+        })
+      }
     </nav>
   )
 };
